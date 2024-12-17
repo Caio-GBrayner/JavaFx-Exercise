23177129c5772e2d40826a1ac1075a53cb8773da
Caio Gomes Brayner <caiogomesbrayner@gmail.com>
2024-12-17 09:25:05 -0300

    exception treatment and sum calculator

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,15 +1,44 @@
 package gui;
 
+import java.util.Locale;
+
+import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
 public class ViewController {
-
+	
 	@FXML
-	private Button btTest;
-
+	private TextField textNumber1;
+	
 	@FXML
-	public void onBtTestAction() {
-		System.out.println("Click");
+	private TextField textNumber2;
+	
+	@FXML
+	private Label labelResult;
+	
+	@FXML
+	private Button btSum;
+
+	
+	@FXML
+	public void onBtSumAction() {
+		try {
+			Locale.setDefault(Locale.US);
+			
+			double number1 = Double.parseDouble(textNumber1.getText());
+			
+			double number2 = Double.parseDouble(textNumber2.getText());
+			
+			double sum = number1 + number2;
+			
+			labelResult.setText(String.format("%.2f", sum));
+		}
+		catch (NumberFormatException e) {
+			Alerts.showAlert("Error", "Parse Error", e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
